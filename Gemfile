97181d400d05dc8e57f731399c8a9db30869775f
Frederick Wong <frederick.kf.wong@gmail.com>
2018-07-21 15:05:32 +0900

    Listings are being shown now

--- Gemfile
+++ Gemfile
@@ -42,6 +42,7 @@ gem 'awesome_print'
 gem 'sidekiq'
 gem 'devise'
 gem "bulma-rails", "~> 0.7.1"
+gem 'will_paginate', '~> 3.1.0'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
-  
+
   require 'sidekiq/web'
   authenticate :user, lambda { |u| u.admin? } do
     mount Sidekiq::Web => '/sidekiq'
   end
 
-  root to: "home#index"
+  root to: "listings#index"
 end
